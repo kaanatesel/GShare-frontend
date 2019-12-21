b57014c7c055c966e8fc4fe4bff1ae32fa25f35c
boracun <58992220+boracun@users.noreply.github.com>
2019-12-21 18:07:27 +0300

    Small Chat Fragments added

--- app/src/main/java/com/example/gshare/LendingChatFragmentSmall.java
+++ app/src/main/java/com/example/gshare/LendingChatFragmentSmall.java
@@ -9,15 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class PinkFragment extends Fragment {
+public class LendingChatFragmentSmall extends Fragment {
 
-    //est comment
-    // test 2
-    // test 3
-    //test onu
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pink, container,false);
+        return inflater.inflate(R.layout.fragment_lending_chat_small, container,false);
     }
 }

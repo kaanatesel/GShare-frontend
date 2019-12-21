e35c19cd5fc725d1728e2337447d496593423192
kaanatesel <kaanatesel99@gmail.com>
2019-12-21 15:44:24 +0300

    test2

--- app/src/main/java/com/example/gshare/PinkFragment.java
+++ app/src/main/java/com/example/gshare/PinkFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 public class PinkFragment extends Fragment {
 
     //test comment
+    // test 2
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {

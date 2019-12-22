f45c34b0b278937d824cb3de2f97fc9be7561754
boracun <58992220+boracun@users.noreply.github.com>
2019-12-22 17:32:55 +0300

    Two popups added

--- app/src/main/java/com/example/gshare/PopupConfirmPublish.java
+++ app/src/main/java/com/example/gshare/PopupConfirmPublish.java
@@ -14,6 +14,6 @@ public class PopupConfirmPublish extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.)
+        return inflater.inflate(R.layout.fragment_popup_confirm_publish, container, false);
     }
 }

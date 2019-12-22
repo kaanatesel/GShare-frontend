304ad23fa06df3504e1aacdce48fc0fad8822e23
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-22 17:47:13 +0300

    Added packages and fragments

--- app/src/main/java/com/example/gshare/Popup/PopupSortByFragment.java
+++ app/src/main/java/com/example/gshare/Popup/PopupSortByFragment.java
@@ -1,4 +1,4 @@
-package com.example.gshare;
+package com.example.gshare.Popup;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,11 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SingleMessageFragment extends Fragment {
+import com.example.gshare.R;
+
+public class PopupSortByFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_single_message, container, false);
+        return inflater.inflate(R.layout.fragment_popup_sort_by, container, false);
     }
 }

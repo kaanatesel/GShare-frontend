bdf3dcf6d3b6671d3948fd6047b440c5843dc03f
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-24 16:14:23 +0300

    Update NoticeEditLendingFragment.java

--- app/src/main/java/com/example/gshare/Notice/NoticeEditLendingFragment.java
+++ app/src/main/java/com/example/gshare/Notice/NoticeEditLendingFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +16,17 @@ import com.example.gshare.R;
 
 public class NoticeEditLendingFragment extends Fragment {
 
+    EditText name;
+    EditText day;
+    EditText g;
+    EditText note;
+    Button editButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notice_edit_lending, container, false);
+        View view = inflater.inflate(R.layout.fragment_notice_edit_lending, container, false);
+
+        return view;
     }
 }

5a19f45c8889a67ce572760e8fbf92a81bba3430
contactLost <50428210+contactLost@users.noreply.github.com>
2019-12-24 03:52:26 +0300

    Create notice yarım kaldı

--- app/src/main/java/com/example/gshare/DBHelper.java
+++ app/src/main/java/com/example/gshare/DBHelper.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.util.Log;
 import android.util.LruCache;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -23,6 +24,7 @@ import com.example.gshare.ModelClasses.User.User;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class DBHelper {
@@ -33,7 +35,8 @@ public class DBHelper {
 
 
     //******************
-    private static User primaryUser;
+    private static User primaryUser; //This is the client User OBJ
+    private static User tempUser;    //This is for temp user objects. Helps callUserByID
 
     private DBHelper(Context context) {
         ctx = context;
@@ -97,7 +100,8 @@ public class DBHelper {
                     public void onResponse(JSONObject response) {
 
                         try {
-                            createUser(response.getString("id"),
+                            createPrimaryUser(
+                                    response.getString("id"),
                                     response.getString("email"),
                                     response.getString("password"),
                                     response.getString("name"),
@@ -124,7 +128,47 @@ public class DBHelper {
         return primaryUser;
     }
 
-    //Helper Methods
+
+
+    public static User callUserByID(int ID){
+
+        String url = "http://35.246.134.158/member/" + ID + "/";
+
+
+        //Create a JSON OBJ request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            tempUser = createUser(response.getString("id"),
+                                    response.getString("email"),
+                                    response.getString("password"),
+                                    response.getString("name"),
+                                    response.getString("createDate")
+                            );
+                        } catch (JSONException e) {
+
+                            Log.e("exception: ", "DBHelper JSON Exception");
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e( "error" , error.toString() ) ;
+
+                    }
+                });
+
+        return  tempUser;
+    }
+
+
+    //Helper Methods for Users
 
     private static void createPrimaryUser(String id, String email, String password, String name, String createDate) {
         primaryUser =  createUser(id,name,password,email,createDate);
@@ -132,16 +176,17 @@ public class DBHelper {
     }
 
     private static User createUser(String id, String name , String password, String email, String createDate){
-        return new User(name,"userNameTODO",password,email,100);
+        return new User(name,"userNameTODO",password,email,100); //TODO
     }
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////GET NOTICE DATA////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    public static ArrayList<Notice> getNotices(){
+    public static ArrayList<Notice> getLendingNotices(){
 
         final ArrayList<Notice> list = new ArrayList<Notice>();
-        String url = "http://www.mocky.io/v2/5dfff58e2f00004b0013b24d";
+        String url = "http://35.246.134.158/demand/findAll/";
 
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url ,null,  new Response.Listener<JSONArray>(){
@@ -149,31 +194,26 @@ public class DBHelper {
             @Override
             public void onResponse(JSONArray response) {
 
-            for(int i = 0 ; i< response.length() ; i++){
+                for(int i = 0 ; i< response.length() ; i++){
 
-                try {
-                    JSONObject jsonObject = response.getJSONObject(i);
-                    Notice tempNotice = new Notice(
-                             jsonObject.getString("name" ),
-                            jsonObject.getInt("day " ),
-                            jsonObject.getString("note"),
-                            jsonObject.getInt("catagory "),
-                            callUserByID(jsonObject.getInt()), //TODO implement here
-                            null );
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Notice tempNotice = new Notice(
+                                jsonObject.getString("name" ),
+                                jsonObject.getInt("day " ),
+                                jsonObject.getString("note"),
+                                jsonObject.getInt("catagory "),
+                                callUserByID( jsonObject.getInt("id") ),
+                                500,                                            //TODO replace with response
+                                null );
 
 
+                        list.add( tempNotice );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
-
-
-
-
-
-
-                list.add( tempNotice );
-            }
 
             }
         }, new Response.ErrorListener() {
@@ -185,8 +225,52 @@ public class DBHelper {
             }
         });
 
-         );
 
+        instance.addToRequestQueue(jsonArrayRequest);
+
+        return list;
+    }
+
+    public static ArrayList<Notice> getBorrowingNotices(){
+
+        final ArrayList<Notice> list = new ArrayList<Notice>();
+        String url = "http://35.246.134.158/demand/findAll/"; //TODO replace the url
+
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url ,null,  new Response.Listener<JSONArray>(){
+
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for(int i = 0 ; i< response.length() ; i++){
+
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Notice tempNotice = new Notice(
+                                jsonObject.getString("name" ),
+                                jsonObject.getInt("day " ),
+                                jsonObject.getString("note"),
+                                jsonObject.getInt("catagory "),
+                                callUserByID( jsonObject.getInt("id") ),
+                                null );
+
+
+                        list.add( tempNotice );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e( "error JSON ARRAY" , error.toString() ) ;
+
+            }
+        });
 
 
         instance.addToRequestQueue(jsonArrayRequest);
@@ -194,9 +278,59 @@ public class DBHelper {
         return list;
     }
 
-    }//DBHelper end
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////CREATE NOTICE IN DB////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void createLendingNotice(Notice notice) {
+
+        String tag_json_obj = "json_obj_req";
+
+        String postURL = "";
+
+        int requesterId ;
+        String productDescription;
+        int categoryId;
 
 
+        //TODO write json by hand
+//        final HashMap<String, String> postParams = new HashMap<String, String>();
+//            postParams.put("requesterId", value1);
+//            postParams.put("productDescription", value2);
+//            postParams.put("categoryId", value3);
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                postURL, new JSONObject(postParams),
+                new com.android.volley.Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+
+        instance.addToRequestQueue(jsonObjReq);
+    }//Lending notice end
+
+
+
+
+
+
+
+
+}//DBHelper end
+
+// POST isteği gönderildikten sonra response alıyor JSON id name email password createDATE
+
+// validateLogin() PU ın id sini kaydetsin
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////Sending Data///////////////////////////////////////////////////
@@ -204,64 +338,6 @@ public class DBHelper {
 
 //Template for JSON file upload
 
-//  final HashMap<Key,Value>{
 
-//}
 
-//    private void postUsingVolley() {
-//        String tag_json_obj = "json_obj_req";
-//
-//        String postURL = "";
-//
-//        final HashMap<String, String> postParams = new HashMap<String, String>();
-//        sendFeedbackParams.put("key1", value1);
-//        sendFeedbackParams.put("key2", value2);
-//        sendFeedbackParams.put("key3", value3);
-//
-//        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-//                postURL, new JSONObject(postParams),
-//                new com.android.volley.Response.Listener<JSONObject>() {
-//
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        //Log.d("TAG", response.toString());
-//                        try {
-//                            //Toast.makeText(mContext, response.getString("message"), Toast.LENGTH_LONG).show();
-//                            Toast.makeText(mContext, "Thank you for your post", Toast.LENGTH_LONG).show();
-//
-//                            if (response.getBoolean("status")) {
-//                                pDialog.dismiss();
-//                                finish();
-//                            }
-//                        } catch (JSONException e) {
-//                            Log.e("TAG", e.toString());
-//                        }
-//                        pDialog.dismiss();
-//                    }
-//                }, new com.android.volley.Response.ErrorListener() {
-//
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                //VolleyLog.d("TAG", "Error: " + error.getMessage());
-//                pDialog.dismiss();
-//                if (isNetworkProblem(error)) {
-//                    Toast.makeText(mContext, "Internet Problem", Toast.LENGTH_SHORT).show();
-//
-//                }
-//            }
-//        }) {
-//
-//            @Override
-//            public String getBodyContentType() {
-//                return "application/json; charset=utf-8";
-//            }
-//
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                return getRequestHeaders();
-//            }
-//        };
-//
-//
-//    }
 //}

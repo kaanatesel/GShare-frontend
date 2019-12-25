232408950ada282c42d0b8cbd1119f7d6aa2a4d1
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-26 01:03:58 +0300

    Update ChatActivity.java

--- app/src/main/java/com/example/gshare/ChatActivity.java
+++ app/src/main/java/com/example/gshare/ChatActivity.java
@@ -11,21 +11,18 @@ import com.example.gshare.Chat.ChatFragment;
 import static com.example.gshare.R.id.linearLayoutForChatsFragment;
 
 public class ChatActivity extends AppCompatActivity {
-    String userName;
-    String password;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        userName = getIntent().getStringExtra("USERNAME");
-        password = getIntent().getStringExtra("PASSWORD");
+        email = getIntent().getStringExtra("email");
 
         ChatFragment chatsFragment = new ChatFragment();
         Bundle bundle = new Bundle();
-        bundle.putString("userName",userName);
-        bundle.putString("password",password);
+        bundle.putString("email",email);
         chatsFragment.setArguments(bundle);
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.linearLayoutForChatsFragment,chatsFragment);

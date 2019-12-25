89967abafe0f34dbfa7be75a2d5ee6fb4d383092
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-25 22:20:40 +0300

    a

--- app/src/main/java/com/example/gshare/HomePageActivity.java
+++ app/src/main/java/com/example/gshare/HomePageActivity.java
@@ -20,11 +20,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 /**
  * This class is not final and will be changed by ONUR
  */
-public class HomePageActivity extends AppCompatActivity  {
+public class HomePageActivity extends AppCompatActivity {
 
-    String userName;
-    String password;
-
+    String email;
 
     ListViewAdapter adapter;
 
@@ -33,87 +31,85 @@ public class HomePageActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        email = getIntent().getStringExtra("USERNAME");
         setContentView(R.layout.blank_layout);
         HomePageFragment fragmentForBeginning = new HomePageFragment();
         Bundle bundle = new Bundle();
-        bundle.putString("userName", userName);
-        bundle.putString("password", password);
+        bundle.putString("email", email);
         fragmentForBeginning.setArguments(bundle);
         FragmentTransaction fragmentTransactionForBeginning = getSupportFragmentManager().beginTransaction();
         fragmentTransactionForBeginning.replace(R.id.main_layout, fragmentForBeginning);
         fragmentTransactionForBeginning.commit();
 
-        //setContentView(R.layout.activity_homepage);
-
-
-        //Wrote for test purposes only
-        userName = getIntent().getStringExtra("USERNAME");
-        password = getIntent().getStringExtra("PASSWORD");
-
 
         BottomNavigationView bottomNav = findViewById(R.id.nav_view);
 
-        bottomNav.setOnNavigationItemSelectedListener( new  BottomNavigationView.OnNavigationItemSelectedListener(){
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-            @Override
-            public boolean onNavigationItemSelected( @NonNull  MenuItem item) {
-                try {
+                                                          @Override
+                                                          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                                              Bundle bundle = new Bundle();
+                                                              bundle.putString("email", email);
+                                                              try {
 
-                    switch (item.getItemId()) {
+                                                                  switch (item.getItemId()) {
 
-                        case R.id.navigation_home:
-
-                                    HomePageFragment fragment1 = new HomePageFragment();
-                                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                                    fragmentTransaction1.replace(R.id.main_layout,fragment1);
-                                    fragmentTransaction1.commit();
-                            return true;
-
-                        case R.id.navigation_Map:
-
-                                    HomePageFragment fragment2 = new HomePageFragment();
-                                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                                    fragmentTransaction2.replace(R.id.main_layout,fragment2);
-                                    fragmentTransaction2.commit();
-                            return true;
-
-                        case R.id.navigation_Notices:
-
-                                    MyNoticesFragment fragment3 = new MyNoticesFragment();
-                                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                                    fragmentTransaction3.replace(R.id.main_layout,fragment3);
-                                    fragmentTransaction3.commit();
-                            return true;
-                        case R.id.navigation_Chat:
-
-                                    ChatFragment fragment4 = new ChatFragment();
-                                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
-                                    fragmentTransaction4.replace(R.id.main_layout,fragment4);
-                                    fragmentTransaction4.commit();
-                            return true;
+                                                                      case R.id.navigation_home:
 
 
-                        case R.id.navigation_Profilet:
-                                    ProfileFragment fragment5 = new ProfileFragment();
-                                    FragmentTransaction fragmentTransaction5 = getSupportFragmentManager().beginTransaction();
-                                    fragmentTransaction5.replace(R.id.main_layout,fragment5);
-                                    fragmentTransaction5.commit();
-                            return true;
-                    }
-                    return false;
+                                                                          HomePageFragment fragment1 = new HomePageFragment();
+                                                                          fragment1.setArguments(bundle);
+                                                                          FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                                                                          fragmentTransaction1.replace(R.id.main_layout, fragment1);
+                                                                          fragmentTransaction1.commit();
+                                                                          return true;
 
-                } catch (NullPointerException e) {
-                    return false;
-                }
+                                                                      case R.id.navigation_Map:
 
-            }
+                                                                          HomePageFragment fragment2 = new HomePageFragment();
+                                                                          fragment2.setArguments(bundle);
+                                                                          FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                                                                          fragmentTransaction2.replace(R.id.main_layout, fragment2);
+                                                                          fragmentTransaction2.commit();
+                                                                          return true;
+
+                                                                      case R.id.navigation_Notices:
+
+                                                                          MyNoticesFragment fragment3 = new MyNoticesFragment();
+                                                                          fragment3.setArguments(bundle);
+                                                                          FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                                                                          fragmentTransaction3.replace(R.id.main_layout, fragment3);
+                                                                          fragmentTransaction3.commit();
+                                                                          return true;
+                                                                      case R.id.navigation_Chat:
+
+                                                                          ChatFragment fragment4 = new ChatFragment();
+                                                                          fragment4.setArguments(bundle);
+                                                                          FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                                                                          fragmentTransaction4.replace(R.id.main_layout, fragment4);
+                                                                          fragmentTransaction4.commit();
+                                                                          return true;
+
+
+                                                                      case R.id.navigation_Profilet:
+                                                                          ProfileFragment fragment5 = new ProfileFragment();
+                                                                          fragment5.setArguments(bundle);
+                                                                          FragmentTransaction fragmentTransaction5 = getSupportFragmentManager().beginTransaction();
+                                                                          fragmentTransaction5.replace(R.id.main_layout, fragment5);
+                                                                          fragmentTransaction5.commit();
+                                                                          return true;
+                                                                  }
+                                                                  return false;
+
+                                                              } catch (NullPointerException e) {
+                                                                  return false;
+                                                              }
+
+                                                          }
 
                                                       }
         );
     }
-
-
-
 
 
 }

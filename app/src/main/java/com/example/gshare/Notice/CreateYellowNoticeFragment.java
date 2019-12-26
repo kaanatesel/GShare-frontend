65d7850613c7903b0ff50ad14127694656889b57
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-26 08:52:48 +0300

    a

--- app/src/main/java/com/example/gshare/Notice/CreateYellowNoticeFragment.java
+++ app/src/main/java/com/example/gshare/Notice/CreateYellowNoticeFragment.java
@@ -145,6 +145,7 @@ public class CreateYellowNoticeFragment extends Fragment implements View.OnClick
         }
 
         HomePageFragment homePageFragment = new HomePageFragment();
+        getActivity().setContentView(R.layout.blank_layout);
         homePageFragment.setArguments(bundle);
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.main_layout, homePageFragment);
